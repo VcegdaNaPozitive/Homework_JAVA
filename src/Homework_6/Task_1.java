b3c3ebde6ac0e40779d87080c55ebc660fa4102b
rsnix <rsnix@bk.ru>
2022-10-26 18:43:18 +0300

    Homework_6

--- src/Homework_6/Task_1.java
+++ src/Homework_6/Task_1.java
@@ -6,14 +6,15 @@ public class Task_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        byte[] array = new byte[n / 8 + 1];
+        byte[] array = new byte[(n - 1) / 8 + 1];
         for (int i = 0; i < n; i++) {
             if (in.nextBoolean()) {
-                array[i / 8] = (byte);
+                array[i / 8] = (byte) (array[i / 8] | (1 << (i % 8)));
             }
         }
         for (int i = 0; i < n; i++) {
-
+            System.out.print((array[i / 8] & (1 << (i % 8))) != 0);
+            System.out.print(" ");
         }
     }
 }
